ae99a686d36fa1fcd8d20493693b7bc3f6c65e2c
Miguel Agueda <miguelmacabral@gmail.com>
2022-01-08 13:26:37 -0800

    Updating documentation.

--- Source/OakD.cpp
+++ Source/OakD.cpp
@@ -1,18 +1,36 @@
+/* Mostly borrowed from https://docs.luxonis.com/projects/api/en/latest/samples/StereoDepth/stereo_depth_video/#stereo-depth-video
+ */
+
 #include "OakD.hpp"
 
+
+/* @brief Class constructor.
+ */
+OakD::OakD()
+{
+    // Empty Constructor.
+}
+
+
+/* @brief Establish a connection to the OakD stereo camera. 
+ * @return Returns true if the connection is successfully established, false otherwise.
+ * @note Currently throws std::runtime_error if no camera is found.
+ * @todo Add try-catch for case when camera is not found, return false when caught.
+ */
 bool OakD::Connect()
 {
     // Define sources and outputs
     this->pipeline = dai::Pipeline();
+
     this->monoLeft = this->pipeline.create<dai::node::MonoCamera>();
     this->monoRight = this->pipeline.create<dai::node::MonoCamera>();
     this->rgbMiddle = this->pipeline.create<dai::node::ColorCamera>();
     this->stereo = this->pipeline.create<dai::node::StereoDepth>();
-    this->stereoOut = this->pipeline.create<dai::node::XLinkOut>();
     this->rgbOut = this->pipeline.create<dai::node::XLinkOut>();
+    this->stereoOut = this->pipeline.create<dai::node::XLinkOut>();
 
-    this->stereoOut->setStreamName("stereo");
     this->rgbOut->setStreamName("rgb");
+    this->stereoOut->setStreamName("stereo");
 
     // Define properties.
     this->monoLeft->setResolution(dai::MonoCameraProperties::SensorResolution::THE_720_P);
@@ -34,9 +52,11 @@ bool OakD::Connect()
     this->monoRight->out.link(this->stereo->right);
     this->stereo->depth.link(this->stereoOut->input);
 
-    this->rgbMiddle->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
     this->rgbMiddle->setBoardSocket(dai::CameraBoardSocket::RGB);
-    this->rgbMiddle->preview.link(this->rgbOut->input);
+    this->rgbMiddle->setVideoSize(1080, 720);
+    this->rgbMiddle->video.link(this->rgbOut->input);
+    this->rgbOut->input.setBlocking(false);
+    this->rgbOut->input.setQueueSize(1);
 
     // Connect to device and start pipeline
     this->device = std::make_shared<dai::Device>(pipeline);
@@ -44,15 +64,28 @@ bool OakD::Connect()
     // Output queue will be used to get the disparity frames from the outputs defined above
     this->rgbOutputQueue = device->getOutputQueue("rgb", 4, true);
     this->stereoOutputQueue = device->getOutputQueue("stereo", 4, true);
+
+    // If control made it this far, connection was successfully established.
     return true;
 }
 
+
+/* @brief Retrieve an RGB image from the OakD center camera.
+ * @return Returns an OpenCV cv::Mat RGB image. 
+ *      Size determined during instance initialization.
+ */
 cv::Mat OakD::GetImage()
 {
     std::shared_ptr<dai::ImgFrame> inDepth = this->rgbOutputQueue->get<dai::ImgFrame>();
     return inDepth->getCvFrame();
 }
 
+
+/* @brief Retrieve a stereo image from the OakD camera.
+ * @return Returns an OpenCV cv::Mat stereo image. 
+ *      Size determined during instance initialization.
+ * @todo Make the returned image hold values that can be correlated to real-life distance in meters.
+ */
 cv::Mat OakD::GetStereoImage()
 {
     std::shared_ptr<dai::ImgFrame> stereoFrame = this->stereoOutputQueue->get<dai::ImgFrame>();
@@ -60,7 +93,12 @@ cv::Mat OakD::GetStereoImage()
 }
 
 
-int OakD::StartStream()
+/* @brief Start a process that will display both RGB and depth images in full resolution.
+ * @note Only way out of this call is for the user to input 'q', or ctrl-c. 
+ * @todo Add conditional flag to while loop that can be accessed from outside loop,
+ *       will allow the stream to be programmatically closed. 
+ */
+void OakD::StartStream()
 {
     while (cv::waitKey(1) != 'q')
     {
@@ -69,6 +107,4 @@ int OakD::StartStream()
         cv::imshow("Frame", frame);
         cv::imshow("Stereo Frame", stereoFrame);
     }
-
-    return 0;
 }
--- Source/OakD.hpp
+++ Source/OakD.hpp
@@ -5,27 +5,21 @@ class OakD
 {
 private:
     dai::Pipeline pipeline;
-    std::shared_ptr<dai::Device> device;
     std::shared_ptr<dai::node::MonoCamera> monoLeft;
     std::shared_ptr<dai::node::MonoCamera> monoRight;
     std::shared_ptr<dai::node::ColorCamera> rgbMiddle;
     std::shared_ptr<dai::node::StereoDepth> stereo;
-    std::shared_ptr<dai::node::XLinkOut> stereoOut;
     std::shared_ptr<dai::node::XLinkOut> rgbOut;
+    std::shared_ptr<dai::node::XLinkOut> stereoOut;
+
+    std::shared_ptr<dai::Device> device;
     std::shared_ptr<dai::DataOutputQueue> rgbOutputQueue;
     std::shared_ptr<dai::DataOutputQueue> stereoOutputQueue;
-
-    // Closer-in minimum depth, disparity range is doubled (from 95 to 190):
-    std::atomic<bool> extended_disparity{false};
-    // Better accuracy for longer distance, fractional disparity 32-levels:
-    std::atomic<bool> subpixel{false};
-    // Better handling for occlusions:
-    std::atomic<bool> lr_check{false};
 
 public:
     OakD(){};
     bool Connect();
     cv::Mat GetImage();
     cv::Mat GetStereoImage();
-    int StartStream();
+    void StartStream();
 };
